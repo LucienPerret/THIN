05a232af3b15cd30c268a9f0df306bdc382d7590
Lucien Perret <lucienperret@icloud.com>
2024-04-07 09:51:00 +0200

    Refactor code to improve performance and readability

---
+++ Stack.hs
@@ -0,0 +1,20 @@
+{-# OPTIONS_GHC #-}
+import Data.Char
+import Debug.Trace (trace, traceShowId)
+
+pushDown :: [Char] -> [Int]
+pushDown xs = calcPDA xs []
+
+calcPDA :: [Char] -> [Int] -> [Int]
+calcPDA [] ys = ys
+calcPDA (x:xs) ys  
+            | isNumber x = calcPDA xs (digitToInt x:ys)
+            | otherwise = calcPDA xs (doCalc (findOperator x) ys)
+
+
+doCalc :: (a -> a -> a) -> [a] -> [a]
+doCalc f ys =  f (head ys) (ys !! 1) : drop 2 ys
+
+findOperator :: Num a => Char -> a -> a -> a
+findOperator f | f == '+' = (+)
+               | f == '*' = (*)
